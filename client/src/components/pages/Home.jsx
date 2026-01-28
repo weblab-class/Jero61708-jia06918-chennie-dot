646240665d377ac39cc8ee8ed41698b498cc74f2
Joanna Liu <joannaliu@DCW27D7PQY.tld>
2026-01-27 21:46:27 -0500

    Changed game name

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -32,7 +32,7 @@ export default function Home() {
       </div>
 
       <main className="homeMain">
-        <h1 className="homeTitle">Keyboard Warriors</h1>
+        <h1 className="homeTitle">Rules You Didn't Read</h1>
         <p className="homeSubtitle">
           Click tiles to infer the hidden rule. Green = correct, Red = incorrect.
         </p>
